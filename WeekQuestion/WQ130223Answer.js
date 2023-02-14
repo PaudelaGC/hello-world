73828e110121154822f47750906d3694406a4db7
PaudelaGC <80599344+PaudelaGC@users.noreply.github.com>
2023-02-14 10:47:43 +0100

    Create WQ130223Answer.js

---
+++ WeekQuestion/WQ130223Answer.js
@@ -0,0 +1,9 @@
+function imprimirDeZeroFinsCen(){
+    let dos = [0,0]
+    let cinc = [0,0,0,0,0]
+    let max = Math.pow(dos.length,dos.length)*Math.pow(cinc.length,dos.length);
+    for(let i = 0; i<=max; i++){
+        console.log(i);
+    }
+}
+imprimirDeZeroFinsCen();
